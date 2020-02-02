45986e7fff341fcec126ff70abc280f901e15b39
Solomon Chu <solomonchu@live.com>
2020-02-02 18:11:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,3 +8,11 @@ end
 def position_taken?(board, index)
   ((board[index] == "X") || (board[index] == "O"))
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
